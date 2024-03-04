338da2dac3534d115dfbc83ce57412765ed6b25c
yisamlee <46322530+yisamlee@users.noreply.github.com>
2024-03-04 21:38:31 +0000

    formatting

--- functions/cul_initcap/cul_initcap.sql
+++ functions/cul_initcap/cul_initcap.sql
@@ -2,66 +2,66 @@ CREATE
 OR REPLACE FUNCTION public.cul_initcap(input_val text) RETURNS text LANGUAGE sql IMMUTABLE STRICT AS $ function $
 SELECT
     replace(
-        replace(
-            replace(
-                replace(
-                    replace(
-                        replace(
-                            replace(
-                                replace(
-                                    replace(
-                                        replace(
-                                            replace(
-                                                replace(
-                                                    regexp_replace(
-                                                        regexp_replace(
-                                                            initcap($ 1),
-                                                            --                          		$$'(([MSTD]|Ve|Re|Ll))([^[:upper:][:lower:]]|$)$$, 
-                                                            --	                      			$$'{@*#!\1!#*@}\3$$, 
-                                                            --                          		'g'
-                                                            '('')(([MSTD]|Ve|Re|Ll))',
-                                                            '\1{@*#!\2!#*@}',
-                                                            'g'
-                                                        ),
-                                                        '(\s?)(Ii|Iii|Iv|Vi|Ix)(?!\w)',
-                                                        '\1{@*#!\2!#*@}',
-                                                        'g'
-                                                    ),
-                                                    '{@*#!M!#*@}',
-                                                    'm'
-                                                ),
-                                                '{@*#!S!#*@}',
-                                                's'
-                                            ),
-                                            '{@*#!T!#*@}',
-                                            't'
-                                        ),
-                                        '{@*#!D!#*@}',
-                                        'd'
-                                    ),
-                                    '{@*#!Ve!#*@}',
-                                    've'
-                                ),
-                                '{@*#!Re!#*@}',
-                                're'
-                            ),
-                            '{@*#!Ll!#*@}',
-                            'll'
-                        ),
-                        '{@*#!Ii!#*@}',
-                        'II'
-                    ),
-                    '{@*#!Iii!#*@}',
-                    'III'
-                ),
-                '{@*#!Iv!#*@}',
-                'IV'
-            ),
-            '{@*#!Vi!#*@}',
-            'VI'
-        ),
-        '{@*#!Ix!#*@}',
-        'IX'
+    replace(
+    replace(
+    replace(
+    replace(
+    replace(
+    replace(
+    replace(
+    replace(
+    replace(
+    replace(
+    replace(
+    regexp_replace(
+    regexp_replace(
+        initcap($ 1),
+    --  $$'(([MSTD]|Ve|Re|Ll))([^[:upper:][:lower:]]|$)$$, 
+    --	$$'{@*#!\1!#*@}\3$$, 
+        'g'
+    '('')(([MSTD]|Ve|Re|Ll))',
+    '\1{@*#!\2!#*@}',
+    'g'
+    ),
+    '(\s?)(Ii|Iii|Iv|Vi|Ix)(?!\w)',
+    '\1{@*#!\2!#*@}',
+    'g'
+    ),
+    '{@*#!M!#*@}',
+    'm'
+    ),
+    '{@*#!S!#*@}',
+    's'
+    ),
+    '{@*#!T!#*@}',
+    't'
+    ),
+    '{@*#!D!#*@}',
+    'd'
+    ),
+    '{@*#!Ve!#*@}',
+    've'
+    ),
+    '{@*#!Re!#*@}',
+    're'
+    ),
+    '{@*#!Ll!#*@}',
+    'll'
+    ),
+    '{@*#!Ii!#*@}',
+    'II'
+    ),
+    '{@*#!Iii!#*@}',
+    'III'
+    ),
+    '{@*#!Iv!#*@}',
+    'IV'
+    ),
+    '{@*#!Vi!#*@}',
+    'VI'
+    ),
+    '{@*#!Ix!#*@}',
+    'IX'
     );
 
 $ function $;
